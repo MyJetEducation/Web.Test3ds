9b0934621a98c44bf5c8dc62ce4d3ddc6ea5474b
dmitriy.b <dmitriy.b@smplt.net>
2022-03-11 11:15:32 +0500

    Remove CORS

--- src/Program.cs
+++ src/Program.cs
@@ -3,16 +3,6 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 	// Add services to the container.
 	IServiceCollection services = builder.Services;
 
-	services.AddCors(options =>
-	{
-		options.AddPolicy("AllowAllHeaders",
-			corsPolicyBuilder =>
-			{
-				corsPolicyBuilder.AllowAnyOrigin()
-					.AllowAnyHeader()
-					.AllowAnyMethod();
-			});
-	});
 	services.AddRazorPages();
 
 	WebApplication app = builder.Build();
@@ -24,19 +14,11 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 		app.UseHsts();
 	}
-
-	app.UseCors(
-		options => options.WithOrigins("http://localhost", "https://localhost")
-			.AllowAnyHeader()
-			.AllowAnyMethod()
-			.AllowCredentials()
-		);
 	
 	app.UseDefaultFiles();
 	app.UseStaticFiles();
 	app.UseForwardedHeaders();
 	app.UseRouting();
-
 	app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
 
 	app.Run();
